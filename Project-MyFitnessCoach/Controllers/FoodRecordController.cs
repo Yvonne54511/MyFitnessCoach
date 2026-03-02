d27f1cfadb31d585d84bc3e28dcc50a98ecf3b41
yvonne42396 <yvonne42396@gmail.com>
2026-03-02 16:47:30 +0800

    加入專案檔案。

---
+++ Project-MyFitnessCoach/Controllers/FoodRecordController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_MyFitnessCoach.Controllers
+{
+	public class FoodRecordController : Controller
+	{
+		public IActionResult ViewFoodRecord()
+		{
+			return View();
+		}
+	}
+}
+ 
