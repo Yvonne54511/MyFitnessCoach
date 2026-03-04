a069e9542a0fc077d7aa01dd9ff1459e1e81f948
yvonne42396 <yvonne42396@gmail.com>
2026-03-04 18:45:44 +0800

    v url:/Users/Login/  v UserController  v LoginDto:Account,Password  v LoginViewModel:Account,Password  v View

---
+++ Project-MyFitnessCoach/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_MyFitnessCoach.Controllers
+{
+	public class UsersController : Controller
+	{
+		
+		public IActionResult Login()
+		{
+			return View();
+		}
+	}
+}
