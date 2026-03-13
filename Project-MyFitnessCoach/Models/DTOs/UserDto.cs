2ff239dc29acb02ea0685624fe459ba836c29d6c
yvonne42396 <yvonne42396@gmail.com>
2026-03-13 11:36:56 +0800

    Cookie InstructorDto UserDto

--- Project-MyFitnessCoach/Models/DTOs/UserDto.cs
+++ Project-MyFitnessCoach/Models/DTOs/UserDto.cs
@@ -1,9 +1,15 @@
-namespace Project_MyFitnessCoach.Models.DTOs
+﻿namespace Project_MyFitnessCoach.Models.Dtos
 {
-    public class UserDto
-    {
-        public int Id { get; set; }
-        public string? UserName { get; set; }
-        public string? Account { get; set; }
-    }
+	public class UserDto
+	{
+		public int Id { get; set; }
+
+		public string Account { get; set; }
+
+		public string Password { get; set; }
+
+		public string UserName { get; set; }
+
+		public string Email { get; set; }
+	}
 }
