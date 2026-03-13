bf33610ed8c29d29cd391a8d6ddf0de1b5fe89b3
yvonne42396 <yvonne42396@gmail.com>
2026-03-13 17:25:26 +0800

    userdto

--- Project-MyFitnessCoach/Models/DTOs/UserDto.cs
+++ Project-MyFitnessCoach/Models/DTOs/UserDto.cs
@@ -3,13 +3,13 @@ namespace Project_MyFitnessCoach.Models.DTOs
 	public class UserDto
 	{
 		public int Id { get; set; }
-
 		public string Account { get; set; }
-
-		public string Password { get; set; }
-
 		public string UserName { get; set; }
-
 		public string Email { get; set; }
+		public string HashedPassword { get; set; }
+        public string? Password { get; set; } // 保留相容性
+        public int? InstructorId { get; set; }
+        public List<string> Roles { get; set; } = new List<string>();
+        public List<string> Functions { get; set; } = new List<string>();
 	}
 }
